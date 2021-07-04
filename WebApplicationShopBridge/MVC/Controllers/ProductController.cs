feadb34859cdbda51bc2af0c72292c4bd7f6cffd
apurva196 <apurvabelhekar412@gmail.com>
2021-07-04 23:02:06 +0530

    adding files

---
+++ WebApplicationShopBridge/MVC/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using MVC.Models;
+
+namespace MVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            IEnumerable<Products> prodList;
+            HttpResponseMessage response = GlobalVariables.webAPIClient.GetAsync("Products").Result;
+            prodList = response.Content.ReadAsAsync<IEnumerable<Products>>().Result;
+            return View(prodList);
+        }
+    }
+}
---
+++ WebApplicationShopBridge/MVC/GlobalVariables.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace MVC
+{
+    public static class GlobalVariables
+    {
+        public static HttpClient webAPIClient = new HttpClient();
+
+          static GlobalVariables()
+        {
+            webAPIClient.BaseAddress = new Uri("https://localhost:44384/api/");
+            webAPIClient.DefaultRequestHeaders.Clear();
+            webAPIClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+        }
+        
+    }
+}
